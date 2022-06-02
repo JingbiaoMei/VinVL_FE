2cb7469b6d655d882eff1c04462bf95f77570d42
JingbiaoMei <jm2245@login-e-3.data.cluster>
2022-06-02 17:49:34 +0100

    final update

--- bash/HMC/attr5050HMCV1.bash
+++ bash/HMC/attr5050HMCV1.bash
@@ -9,3 +9,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --time=4:00:00
 python -m torch.distributed.launch --nproc_per_node=4 ./tools/test_sg_net.py --config-file ./sgg_configs/vgattr/HMC/vinvl_attr5050.yaml
+
